2b244c6e4c3a296db6cb28e75976a9b37ed3f933
Moumou38 <moumou38@msn.com>
2013-10-23 20:18:08 +0200

    Player control update
    L'objet se déplace suivant la direction vers laquelle la caméra pointe.

--- Assets/BallPlayer.cs
+++ Assets/BallPlayer.cs
@@ -11,8 +11,30 @@ public class BallPlayer : MonoBehaviour {
         canBeTaken = null;
 	}
 	
+	
 	// Update is called once per frame
 	void Update () {
+		
+		var controlCameraObject = GameObject.Find("Main Camera");
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetAxis("Left Analog Vertical") < -0.2f)
+            rigidbody.AddForce(controlCameraObject.transform.forward * 3);
+        
+        else if (Input.GetKey(KeyCode.DownArrow) || Input.GetAxis("Left Analog Vertical") > 0.2f)
+            rigidbody.AddForce(-1 * controlCameraObject.transform.forward * 3);
+
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetAxis("Left Analog Horizontal") < -0.2f)
+            rigidbody.AddForce(controlCameraObject.transform.right * 3 * -1 );
+        else if (Input.GetKey(KeyCode.RightArrow) || Input.GetAxis("Left Analog Horizontal") > 0.2f)
+            rigidbody.AddForce(controlCameraObject.transform.right * 3);
+
+
+
+        if (Input.GetKeyDown(KeyCode.E) || Input.GetButtonDown("X"))
+            GrabObject();
+        
+		
+		
+		/*
 		if(Input.GetKey(KeyCode.LeftArrow) || Input.GetAxis("Left Analog Horizontal") < -0.2f)
 			rigidbody.AddForce(Vector3.left);
 		else if(Input.GetKey(KeyCode.RightArrow) || Input.GetAxis("Left Analog Horizontal") > 0.2f)
@@ -27,6 +49,7 @@ public class BallPlayer : MonoBehaviour {
             GrabObject();
        // if (objectTaken != null)
        //     objectTaken.rigidbody.MovePosition(move);
+       */
 	}
 
     void GrabObject()
