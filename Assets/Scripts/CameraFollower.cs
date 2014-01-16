4594de4d4afa1f2e2ec51058956398524c84461d
MrFraggy <project.eww@gmail.com>
2014-01-16 18:56:12 +0100

    Room1 works!

--- Assets/Scripts/CameraFollower.cs
+++ Assets/Scripts/CameraFollower.cs
@@ -4,11 +4,12 @@ using System.Collections;
 public class CameraFollower : MonoBehaviour {
 	
 	public GameObject ball;
-	float theta, phi;
+	float theta, phi, maxDist;
 	// Use this for initialization
 	void Start () {
 		theta = 0;
 		phi = 0;
+        maxDist = Constants.camDist;
 	}
 	
 	// Update is called once per frame
@@ -24,7 +25,7 @@ public class CameraFollower : MonoBehaviour {
 
 
             if (tmp1 > 0.2 || tmp1 < -0.2)
-                phi -= 2 * tmp1;
+                phi += 2 * tmp1;
 
             if (phi >= 179.9f)
                 phi = 179.9f;
@@ -37,6 +38,7 @@ public class CameraFollower : MonoBehaviour {
         {
             float tmp = Input.GetAxis("Mouse X") * 0.8f;
             float tmp1 = Input.GetAxis("Mouse Y") * 0.8f;
+
 
             if (tmp > 0.2f || tmp < -0.2f)
                 theta += 2 * tmp;
@@ -53,14 +55,39 @@ public class CameraFollower : MonoBehaviour {
         }
 		
 		
-		float x = Constants.camDist*Mathf.Cos(theta*Mathf.PI/180f)*Mathf.Sin(phi*Mathf.PI/180f);
-		float z = Constants.camDist*Mathf.Sin(theta*Mathf.PI/180f)*Mathf.Sin(phi*Mathf.PI/180f);
-		float y = Constants.camDist*Mathf.Cos(phi*Mathf.PI/180f);
+		
 		
 		Vector3 look = ball.transform.position;
-		look.y += 7; 
-		transform.position = look-new Vector3(x,y,z);
+		look.y += 7;
+        
+		transform.position = look-CalculateOrbit(Constants.camDist);
 		
 		transform.LookAt(look);
 	}
+
+    void OnCollisionEnter(Collision h)
+    {
+       /* Debug.Log("toto");
+        RaycastHit hit;
+        Vector3 dir = transform.position - ball.transform.position;
+        if(Physics.Raycast(ball.transform.position, dir, out hit)) {
+            Debug.Log("collide");
+            Vector3 look = ball.transform.position;
+            transform.position = look-CalculateOrbit(hit.distance-10f);
+            maxDist = hit.distance;
+            //transform.position = ball.transform.position;// (hit.point - ball.transform.position) * 0.8f + ball.transform.position;
+        }
+        else
+        {
+            maxDist = Constants.camDist;
+        }*/
+    }
+
+    Vector3 CalculateOrbit(float dist)
+    {
+        float x = dist * Mathf.Cos(theta * Mathf.PI / 180f) * Mathf.Sin(phi * Mathf.PI / 180f);
+        float z = dist * Mathf.Sin(theta * Mathf.PI / 180f) * Mathf.Sin(phi * Mathf.PI / 180f);
+        float y = dist * Mathf.Cos(phi * Mathf.PI / 180f);
+        return new Vector3(x, y, z);
+    }
 }
--- Assets/Scripts/Room1Script.cs
+++ Assets/Scripts/Room1Script.cs
@@ -12,6 +12,7 @@ public class Room1Script : MonoBehaviour {
 	public GameObject[] cubes;
 	public GameObject etagere;
 	public GameObject roomCenter;
+    public GameObject pillar;
 	public bool startRoom;
 	
 	// Use this for initialization
@@ -25,9 +26,10 @@ public class Room1Script : MonoBehaviour {
 		player = GameObject.Find("Player");
 		etagere = GameObject.Find("Etagere");
 		roomCenter = GameObject.Find("RoomCenter");
+        pillar = GameObject.Find("Pillar");
 		startRoom = false;
-		lastAngle = 0;//Vector3.Angle(player.transform.position, roomCenter.transform.position);
-		loop = 0;
+		lastAngle = Vector3.Angle(player.transform.position, roomCenter.transform.position);
+		loop = -1;
 		updateLoopValue = false;
 	}
 	
@@ -36,22 +38,102 @@ public class Room1Script : MonoBehaviour {
 		if(!startRoom)
 			return;
 
-		currAngle = Vector3.Angle(player.transform.position, roomCenter.transform.position);
-		
-		if(updateLoopValue) {
-			if(currAngle > lastAngle) {
-				
-			} else {
-				
-			}
-			
-		}
-		//lastAngle = currAngle;
+        Vector3 pos = player.transform.position - roomCenter.transform.position;
+        currAngle = Constants.RealAngle(pos, -roomCenter.transform.right, -roomCenter.transform.forward);
+        IncreaseLoop();
+        loopUpdate();
+		lastAngle = currAngle;
 	}
 	
 	void IncreaseLoop() {
-		
+        if (lastAngle > 350 && currAngle < 50)
+            loop++;
+        else if (currAngle > 350 && lastAngle < 50)
+            loop--;
 	}
+
+    void loopUpdate()
+    {
+        if (loop == 0 && currAngle < 20)
+        {
+            etagere.renderer.enabled = false;
+            etagere.collider.enabled = false;
+        } else if(loop == -1 && currAngle > 350) {
+            etagere.renderer.enabled = true;
+            etagere.collider.enabled = true;
+        }
+        
+        if (loop == 2 && currAngle > 350)
+        {
+            etagere.renderer.enabled = false;
+            etagere.collider.enabled = false;
+        }
+        else if (loop == 3 && currAngle < 20)
+        {
+            etagere.renderer.enabled = true;
+            etagere.collider.enabled = true;
+        }
+
+        for (int i = 0; i < 3; ++i)
+        {
+            if (loop == i && currAngle > 160)
+            {
+                cubes[i].renderer.enabled = false;
+                cubes[i].collider.enabled = false;
+            }
+            else if (loop == i && currAngle < 140)
+            {
+                cubes[i].renderer.enabled = true;
+                cubes[i].collider.enabled = true;
+            }
+        }
+
+
+        if (loop == -1 && currAngle < 240)
+        {
+            doorEnd.transform.Find("door").renderer.enabled = true;
+            doorEnd.collider.enabled = true;
+        }
+        else if (loop == -1 && currAngle > 240)
+        {
+            doorEnd.transform.Find("door").renderer.enabled = false;
+            doorEnd.collider.enabled = false;
+        }
+
+        if (loop == 2 && currAngle > 240)
+        {
+            doorEnd.transform.Find("door").renderer.enabled = true;
+            doorEnd.collider.enabled = true;
+        }
+        else if (loop == 2 && currAngle < 240)
+        {
+            doorEnd.transform.Find("door").renderer.enabled = false;
+            doorEnd.collider.enabled = false;
+        }
+
+
+        if (loop == 0 && currAngle < 50)
+        {
+            doorStart.transform.Find("door").renderer.enabled = true;
+            doorStart.collider.enabled = true;
+        }
+        else if(loop == 0 && currAngle > 50)
+        {
+            doorStart.transform.Find("door").renderer.enabled = false;
+            doorStart.collider.enabled = false;
+        }
+
+        if (loop == 3 && currAngle > 50)
+        {
+            doorStart.transform.Find("door").renderer.enabled = true;
+            doorStart.collider.enabled = true;
+        }
+        else if (loop == 3 && currAngle < 50)
+        {
+            doorStart.transform.Find("door").renderer.enabled = false;
+            doorStart.collider.enabled = false;
+        }
+    }
 	
 	void OnTriggerEnter() {
 		startRoom = true;
