2cb3b142d8786e93e1837a0766a6fc2c64697980
Moumou38 <moumou38@msn.com>
2014-02-02 09:42:19 +0100

    ajout seed foireuse

--- Assets/Scripts/StreetGenerator.cs
+++ Assets/Scripts/StreetGenerator.cs
@@ -119,6 +119,8 @@ public class StreetGenerator {
         /*Random.seed = 1039656428;*/
         /* Random.seed = -331318947;*/
         // Random.seed = -2098638175; 
+
+		//Random.seed = 508101336;
         Debug.Log("Seed: " + Random.seed);
         
 		placed.Clear();
