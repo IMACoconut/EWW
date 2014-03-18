93eb970ffe46a25d1019419418283b60c9865947
Moumou38 <moumou38@msn.com>
2014-03-18 13:22:19 +0100

    poussage nouvelle salle + correction (WIP)

---
+++ Assets/Scripts/RoomTest2_switch.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomTest2_switch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/RoomTest2_teleport.cs
+++ Assets/Scripts/RoomTest2_teleport.cs
@@ -4,24 +4,25 @@ using System.Collections;
 public class RoomTest2_teleport : MonoBehaviour {
 
     private GameObject player;
-    private GameObject teleporter1, teleporter2;
+    private GameObject teleporter1, teleporter2, target1;
 
 	// Use this for initialization
 	void Start () {
 
         player = GameObject.Find("Player");
-        teleporter2 = GameObject.Find("teleporter2");
+        target1 = GameObject.Find("target1");
 
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        if (this.name == "teleporter2") { this.GetComponentInChildren<Renderer>().material.color = Color.red; }
 	
 	}
 
     void OnTriggerEnter(Collider player)
     {
-        if (player.tag.Equals("Player")) player.transform.position = teleporter2.transform.position;
+        if (player.tag.Equals("Player")) player.transform.position = target1.transform.position;
     }
 }
