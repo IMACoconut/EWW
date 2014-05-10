f4f55b5159950ecd963dde0df01a365f242ae943
Moumou38 <moumou38@msn.com>
2014-05-10 16:55:41 +0200

    menu + props

---
+++ Assets/Scripts/flicker.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flicker : MonoBehaviour {
+
+	private Light lightclaveau;
+	private float intensity ;
+	private int up;
+	private float timer; 
+	// Use this for initialization
+	void Start () {
+		intensity = 0.7f ;
+		lightclaveau = this.light; 
+		up = 1 ;
+		timer = 0f; 
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timer > 0.15f)
+		{
+			intensity += up * 0.1f;
+			lightclaveau.intensity = intensity;
+			if (intensity > 0.4) up = -1;
+			else if (intensity < 1.3) up = 1;
+		}
+		
+		
+		timer += Time.deltaTime;
+		
+		
+		
+	}
+}
